cf225ec01b49bfb0982b4a1fee0079a85ba548ff
ash-gandhi19 <91827704+ash-gandhi19@users.noreply.github.com>
2022-07-04 23:03:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,22 +13,30 @@ const answersRoute = require("./routes/answers.route");
 
 const main = async () => {
   
-
+try{
   // Connecting to mongoDb Atlas
   mongoose.connect(process.env.MONGO_URL);
 
   const app = express();
-var port = normalizePort(process.env.PORT);
+var PORT = normalizePort(process.env.PORT);
   // Middlewares
+ 
+   app.set("views", path.join(__dirname, "views"));
+    app.set("view engine", "jade");
+    app.use(express.urlencoded({ extended: false }));
+    app.use(cookieParser());
+    app.use(express.static(path.join(__dirname, "public")));
+    app.use(express.json());
+app.use(helmet());
+  app.use(morgan("common"));
   app.use(
     cors({
       origin: process.env.FRONTEND,
     })
   );
 
-  app.use(express.json());
-  app.use(helmet());
-  app.use(morgan("common"));
+  
+  
 
   app.use("/auth", authRoute);
 
@@ -39,9 +47,18 @@ var port = normalizePort(process.env.PORT);
   app.use("/comments", commentsRoute);
 
   app.use("/answers", answersRoute);
-  app.listen(port, () => {
+ /* app.listen(port, () => {
     console.log(`API is ready on http://localhost:${port}`);
-  });
+  });*/
+  app.listen(PORT, () => {
+      console.log(`API is ready on http://localhost:${PORT}`);
+    });
+
+ } catch (error) {
+    console.log("error starting our application", error);
+  }
 };
 
 main();
+
+  
