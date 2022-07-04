71e24f35005b13ecb30342d7cd824c266f9ff2e4
ash-gandhi19 <91827704+ash-gandhi19@users.noreply.github.com>
2022-07-04 23:07:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -60,5 +60,19 @@ app.use(helmet());
 };
 
 main();
+function normalizePort(val) {
+  var port = parseInt(val, 10);
 
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
   
